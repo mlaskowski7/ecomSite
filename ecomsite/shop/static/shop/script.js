c411cfa14f811faeadc09c81edf41efe43da2a6c
mlaskowski7 <mtlaskowski7@gmail.com>
2023-12-06 17:29:40 +0100

    adding cart js functionality

---
+++ ecomsite/shop/static/shop/script.js
@@ -0,0 +1,24 @@
+
+let cart;
+
+if(localStorage.getItem('cart') == null){
+    cart = {};
+} else{
+    cart = JSON.parse(localStorage.getItem('cart'));
+}
+
+$(document).on('click','.atc', (event) =>{
+    console.log('atc clicked');
+    let item_id = event.target.id.toString();
+    console.log(item_id);
+
+    if(cart[item_id] != undefined){
+        cart[item_id] += 1;
+    } else{
+        cart[item_id] = 1;
+    }
+    console.log(cart);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    document.getElementById("cart").innerHTML = "Cart (" + Object.keys(cart).length + ")";
+    
+});
