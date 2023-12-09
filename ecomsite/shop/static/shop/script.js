d01677b453a3ac09be43d289f943865e993380a0
mlaskowski7 <mtlaskowski7@gmail.com>
2023-12-09 14:16:13 +0100

    commit

--- ecomsite/shop/static/shop/script.js
+++ ecomsite/shop/static/shop/script.js
@@ -1,24 +1,24 @@
+// console.log("working");
+// let cart;
 
-let cart;
+// if(localStorage.getItem('cart') == null){
+//     cart = {};
+// } else{
+//     cart = JSON.parse(localStorage.getItem('cart'));
+// }
 
-if(localStorage.getItem('cart') == null){
-    cart = {};
-} else{
-    cart = JSON.parse(localStorage.getItem('cart'));
-}
+// $(document).on('click','.atc', (event) =>{
+//     console.log('atc clicked essa');
+//     let item_id = event.target.id.toString();
+//     console.log(item_id);
 
-$(document).on('click','.atc', (event) =>{
-    console.log('atc clicked');
-    let item_id = event.target.id.toString();
-    console.log(item_id);
-
-    if(cart[item_id] != undefined){
-        cart[item_id] += 1;
-    } else{
-        cart[item_id] = 1;
-    }
-    console.log(cart);
-    localStorage.setItem('cart', JSON.stringify(cart));
-    document.getElementById("cart").innerHTML = "Cart (" + Object.keys(cart).length + ")";
+//     if(cart[item_id] != undefined){
+//         cart[item_id] += 1;
+//     } else{
+//         cart[item_id] = 1;
+//     }
+//     console.log(cart);
+//     localStorage.setItem('cart', JSON.stringify(cart));
+//     document.getElementById("cart").innerHTML = "Cart (" + Object.keys(cart).length + ")";
     
-});
+// });
